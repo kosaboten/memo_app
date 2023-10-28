8aa6cd057522e1fb5764b021fdc6660cd7d3e850
kosaboten <saboten.system@gmail.com>
2023-10-28 17:53:35 +0900

    メモの一覧と詳細の表示機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Memoクラスを読み込む
+use App\Models\Memo;
 
 class MemoController extends Controller
 {
-    //
+    public  function index()
+    {
+        $memos = Memo::all();
+        return view('memos.index', ['memos' => $memos]);
+    }
+    // showページへ移動
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
 }
